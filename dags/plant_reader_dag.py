5f1a4eb70c3cf10812a4401a8d1400dd3bb05a62
pol <pol.monso@somenergia.coop>
2022-12-02 18:09:04 +0100

    update dag to modern CDD

--- dags/plant_reader_dag.py
+++ dags/plant_reader_dag.py
@@ -4,8 +4,7 @@ from airflow.providers.docker.operators.docker import DockerOperator
 from util_tasks.t_branch_pull_ssh import build_branch_pull_ssh_task
 from util_tasks.t_git_clone_ssh import build_git_clone_ssh_task
 from util_tasks.t_check_repo import build_check_repo_task
-from util_tasks.t_image_build import build_image_build_task
-from util_tasks.t_remove_image import build_remove_image_task
+from util_tasks.t_update_docker_image import build_update_image_task
 from docker.types import Mount, DriverConfig
 from datetime import datetime, timedelta
 from airflow.models import Variable
@@ -39,23 +38,24 @@ driver_config = DriverConfig(name='local', options=nfs_config)
 mount_nfs = Mount(source="local", target="/repos", type="volume", driver_config=driver_config)
 
 
-with DAG(dag_id='plant_reader_dag', start_date=datetime(2022,9,26), schedule_interval='*/5 * * * *', catchup=False, tags=["Dades", "Plantmonitor"], default_args=args) as dag:
+with DAG(dag_id='plant_reader_dag', start_date=datetime(2022,12,2), schedule_interval='*/5 * * * *', catchup=False, tags=["Dades", "Plantmonitor"], default_args=args) as dag:
 
     repo_name = 'modbus-reader'
 
-    task_branch_pull_ssh = build_branch_pull_ssh_task(dag=dag, task_name='plant_reader', repo_name=repo_name)
-    task_git_clone = build_git_clone_ssh_task(dag=dag, repo_name=repo_name)
     task_check_repo = build_check_repo_task(dag=dag, repo_name=repo_name)
-    task_image_build = build_image_build_task(dag=dag, repo_name=repo_name)
-    task_remove_image = build_remove_image_task(dag=dag, repo_name=repo_name)
+    task_git_clone = build_git_clone_ssh_task(dag=dag, repo_name=repo_name)
+    task_branch_pull_ssh = build_branch_pull_ssh_task(dag=dag, task_name='plant_reader', repo_name=repo_name)
+    task_update_image = build_update_image_task(dag=dag, repo_name=repo_name)
 
     plant_reader_task = DockerOperator(
         api_version='auto',
         task_id='plant_reader',
-        image=f'{repo_name}-requirements:latest',
+        docker_conn_id='somenergia_registry',
+        image='{}/{}-requirements:latest'.format('{{ conn.somenergia_registry.host }}', repo_name),
         working_dir=f'/repos/{repo_name}',
-        command='python3 -m scripts.main get-readings "{{ var.value.plantlake_dbapi }}" modbus_readings planta-asomada.somenergia.coop 1502 3 input 0 52 151 8',
-        docker_url=Variable.get("moll_url"),
+        command='python3 -m scripts.main get-readings "{{ var.value.plantlake_dbapi }}"\
+                 modbus_readings planta-asomada.somenergia.coop 1502 3 input 0 52 151 8',
+        docker_url=Variable.get("generic_moll_url"),
         mounts=[mount_nfs],
         mount_tmp_dir=False,
         auto_remove=True,
@@ -65,7 +65,7 @@ with DAG(dag_id='plant_reader_dag', start_date=datetime(2022,9,26), schedule_int
 
     task_check_repo >> task_git_clone
     task_check_repo >> task_branch_pull_ssh
-    task_git_clone >> task_image_build
+    task_git_clone >> task_update_image
     task_branch_pull_ssh >> plant_reader_task
-    task_branch_pull_ssh >> task_remove_image
-    task_remove_image >> task_image_build >> plant_reader_task
+    task_branch_pull_ssh >> task_update_image
+    task_update_image >> plant_reader_task
