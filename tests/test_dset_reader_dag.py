8b4628fab3b044ac537fce350a888c617dbc54d5
pol <pol.monso@somenergia.coop>
2023-08-18 16:49:12 +0200

    add dag syntax testing

---
+++ tests/test_dset_reader_dag.py
@@ -0,0 +1,15 @@
+import os
+import pytest
+from airflow.models import DagBag
+
+@pytest.fixture()
+def dagbag():
+    os.environ["AIRFLOW_VAR_AVAILABLE_MOLLS"] = "fakemoll.somenergia.coop"
+    return DagBag(dag_folder='dags',include_examples=False)
+
+def test_dag_loaded(dagbag):
+    dag = dagbag.get_dag(dag_id="dset_historic_reader_dag")
+    assert dagbag.import_errors == {}
+    assert dag is not None
+    assert len(dag.tasks) == 1
+
