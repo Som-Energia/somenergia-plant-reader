39138b67352200e2459d310a868880f0b3390eb1
pol <pol.monso@somenergia.coop>
2023-08-22 16:47:00 +0200

    add timezone to to_date parameter

--- scripts/read_dset_api.py
+++ scripts/read_dset_api.py
@@ -59,7 +59,11 @@ def get_historic_readings(
         help="timestamp with timezone, inclusive.",
         formats=["%Y-%m-%dT%H:%M:%S%z", "%Y-%m-%d %H:%M:%S%z", "%Y-%m-%d", "%Y-%m-%dT%H:%M:%S", "%Y-%m-%d %H:%M:%S"]
     ),
-    to_date: datetime.datetime = typer.Option(..., help="timestamp with timezone, not inclusive."),
+    to_date: datetime.datetime = typer.Option(...,
+        help="timestamp with timezone, not inclusive.",
+                help="timestamp with timezone, inclusive.",
+        formats=["%Y-%m-%dT%H:%M:%S%z", "%Y-%m-%d %H:%M:%S%z", "%Y-%m-%d", "%Y-%m-%dT%H:%M:%S", "%Y-%m-%d %H:%M:%S"]
+    ),
     schema: str = typer.Option("public", "--schema"),
 ):
     # hack to make it not inclusive...
