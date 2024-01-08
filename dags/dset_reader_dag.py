397beb73ec9aabc83a1bb9a0e3ae33fd24e68ec2
Pol Monsó Purtí <pol.monso@somenergia.coop>
2024-01-08 17:05:40 +0100

    feature: Add daily dset reader (for meters) (et/somenergia-plant-reader!14)
    * add catchup true to see if we get the previous day correctly
    
    * add daily dset reader dag pointing to lake_test

--- dags/dset_reader_dag.py
+++ dags/dset_reader_dag.py
@@ -139,5 +139,65 @@ with DAG(
         force_pull=True,
     )
 
+
+dset_daily_dag_doc = """
+### Daily dag
+
+Els comptadors arriben amb un dia de retard. Aquest és un dag diari que fa exactament el mateix que el
+dag històric precedent, però amb un offset d'un dia.
+
+Després tenim un camí en el nostre pipe que materialitza un dia més tard.
+"""
+
+with DAG(
+    dag_id="dset_historic_reader_daily_dag",
+    start_date=datetime(2023, 11, 1),
+    schedule="0 5 * * *",
+    catchup=True,
+    tags=["Dades", "Plantmonitor", "Ingesta"],
+    default_args=args_with_retries,
+    max_active_runs=5,
+    doc_md=dset_daily_dag_doc
+) as dag:
+    repo_name = "somenergia-plant-reader"
+
+    sampled_moll = get_random_moll()
+
+    # e.g. data_interval_start from airflow 2023-08-13T00:00:00+00:00
+    # isoformat utc
+
+    dset_reader_task = DockerOperator(
+        api_version="auto",
+        task_id="dset_plant_reader_daily",
+        docker_conn_id="somenergia_harbor_dades_registry",
+        image="{}/{}-app:latest".format(
+            "{{ conn.somenergia_harbor_dades_registry.host }}",
+            repo_name,
+        ),
+        working_dir=f"/repos/{repo_name}",
+        command=(
+            "python3 -m scripts.read_dset_api get-historic-readings"
+            " --db-url {{ var.value.plantmonitor_db }}"
+            " --api-base-url {{ var.value.dset_url }}"
+            " --endpoint /api/data"
+            " --api-key {{ var.value.dset_apikey }}"
+            " --from-date {{ data_interval_start }}"
+            " --to-date {{ data_interval_end }}"
+            " --schema lake_test"
+            " --sig-detail"
+            " --apply-k-value"
+            " --query-timeout {{ var.value.get('dset_query_timeout_seconds', 40) | int }}"
+            " --request-time-offset-min 305"
+        ),
+        docker_url=sampled_moll,
+        mounts=[mount_nfs],
+        mount_tmp_dir=False,
+        auto_remove="force",
+        retrieve_output=True,
+        trigger_rule="none_failed",
+        force_pull=True,
+    )
+
+
 if __name__ == "__main__":
     dag.test()
